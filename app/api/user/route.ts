a791709955f413dbd67532dc18bc882b56336544
sandeshrajbhandari <sandeshrb87@gmail.com>
2023-11-01 20:25:29 +1000

    user registration final.

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,5 +1,6 @@
-import { NextResponse } from "next/server"
-
+import { NextResponse } from "next/server";
+import { db } from "../../lib/db";
+import { hash } from "bcrypt";
 // export async function GET() {
 //   return NextResponse.json({ success: true })
 // }
@@ -7,8 +8,55 @@ import { NextResponse } from "next/server"
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    return NextResponse.json(body);
-  } catch (error) {
+    const { email, username, password } = body;
 
+    // check if email already exists
+    const existingUserByEmail = await db.user.findUnique({
+      where: { email: email },
+    });
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "User with this email already exists",
+        },
+        { status: 409 }
+      );
+    }
+
+    // chec if username already exists
+    const existingUserByUsername = await db.user.findUnique({
+      where: { username: username },
+    });
+    if (existingUserByUsername) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "User with this username already exists",
+        },
+        { status: 409 }
+      );
+    }
+    const hashedPassword = await hash(password, 10);
+    const newUser = await db.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+    const { password: newUserPassword, ...rest } = newUser;
+    // doing the line above to pass user info without the password.
+    return NextResponse.json(
+      { user: rest, message: "User created successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "User created successfully." },
+      {
+        status: 201,
+      }
+    );
   }
 }
